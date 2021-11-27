e94d4ad24683bc716927b834fcfc359a739b2a27
moduhan <devduhanmo@gmail.com>
2021-11-28 00:19:58 +0900

    refactor: 로그출력 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,13 +8,11 @@ public class Application {
         Game game = new Game();
         Util util = new Util();
         List<Integer> computer = util.initComputer();
-        System.out.println(computer);
         while (true) {
             List<Integer> input = util.input();
             String result = game.play(input, computer);
             if (result.equals("1")) {
                 computer = util.initComputer();
-                System.out.println(computer);
             } else if (result.equals("2")) {
                 return;
             } else if (!result.equals("")) {
