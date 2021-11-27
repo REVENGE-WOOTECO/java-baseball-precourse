7f622db0c07cd2594833a01e92beab3f4b94c523
moduhan <devduhanmo@gmail.com>
2021-11-28 00:17:42 +0900

    fix: 종료 커맨드를 눌러도 종료가 되지 않는 버그 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,12 +1,15 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 import static baseball.Util.BASE_COUNT;
 
 public class Game {
 
-    int play(List<Integer> input, List<Integer> computer) {
+    String play(List<Integer> input, List<Integer> computer) {
+        String result = "";
         int strike = 0;
         int ball = 0;
         for (int pos = 0; pos < BASE_COUNT; pos++) {
@@ -20,8 +23,9 @@ public class Game {
         if (strike == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
                     + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            result = Console.readLine();
         }
-        return strike;
+        return result;
     }
 
     private void printResult(int strike, int ball) {
