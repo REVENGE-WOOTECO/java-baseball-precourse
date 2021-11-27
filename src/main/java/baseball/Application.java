42dc37daba204cdc3a8e8cb33931669d7294bf08
moduhan <devduhanmo@gmail.com>
2021-11-28 00:18:18 +0900

    feat: 실제 게임진행 반복문 구현, 정답 후 재시작/종료 구분 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        Game game = new Game();
+        Util util = new Util();
+        List<Integer> computer = util.initComputer();
+        System.out.println(computer);
+        while (true) {
+            List<Integer> input = util.input();
+            String result = game.play(input, computer);
+            if (result.equals("1")) {
+                computer = util.initComputer();
+                System.out.println(computer);
+            } else if (result.equals("2")) {
+                return;
+            } else if (!result.equals("")) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
