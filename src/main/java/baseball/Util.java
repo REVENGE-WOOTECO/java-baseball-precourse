0d31c1f3af1f719d543aaf721bb3f05059fefd4c
moduhan <devduhanmo@gmail.com>
2021-11-27 20:24:44 +0900

    feat: 사용자의 입력값 저장 기능과 입력값 유효성 검사 기능 구현

--- src/main/java/baseball/Util.java
+++ src/main/java/baseball/Util.java
@@ -1,9 +1,13 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Util {
 
@@ -18,5 +22,38 @@ public class Util {
             }
         }
         return computer;
+    }
+
+    List<Integer> input() {
+        String input = Console.readLine();
+        validate(input);
+        return Arrays.stream(input.split("")).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    void validate(String input) {
+        if (!validateOneToNine(input)) {
+            throw new IllegalArgumentException();
+        }
+        if (!validateRange(input)) {
+            throw new IllegalArgumentException();
+        }
+        if (!validateDuplicate(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    boolean validateDuplicate(String input) {
+        return input.charAt(0) != input.charAt(1)
+                && input.charAt(0) != input.charAt(2)
+                && input.charAt(1) != input.charAt(2);
+    }
+
+    boolean validateRange(String input) {
+        return input.length() <= 3 && input.length() >= 1;
+    }
+
+    boolean validateOneToNine(String input) {
+        String pattern = "^[1-9]*$";
+        return Pattern.matches(pattern, input);
     }
 }
