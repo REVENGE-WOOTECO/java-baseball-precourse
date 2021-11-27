d0ede7123a4b99f427e7fb5fda0d58c099a45822
moduhan <devduhanmo@gmail.com>
2021-11-27 23:40:32 +0900

    feat: 실제 사용자의 입력값과 컴퓨터의 숫자를 비교 후 볼,스트라이크 갯수 출력 기능 구현

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,46 @@
+package baseball;
+
+import java.util.List;
+
+import static baseball.Util.BASE_COUNT;
+
+public class Game {
+
+    int play(List<Integer> input, List<Integer> computer) {
+        int strike = 0;
+        int ball = 0;
+        for (int pos = 0; pos < BASE_COUNT; pos++) {
+            if (isStrike(pos, input.get(pos), computer)) {
+                strike++;
+            } else if (isBall(input.get(pos), computer)) {
+                ball++;
+            }
+        }
+        printResult(strike, ball);
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
+                    + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+        return strike;
+    }
+
+    private void printResult(int strike, int ball) {
+        if (strike != 0 && ball != 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        } else if (strike != 0) {
+            System.out.println(strike + "스트라이크");
+        } else if (ball != 0) {
+            System.out.println(ball + "볼");
+        } else {
+            System.out.println("낫싱");
+        }
+    }
+
+    private boolean isStrike(int pos, int num, List<Integer> computer) {
+        return computer.indexOf(num) == pos;
+    }
+
+    private boolean isBall(int num, List<Integer> computer) {
+        return computer.contains(num);
+    }
+}
