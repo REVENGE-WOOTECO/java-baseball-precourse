cc1a67082411206cf1505bbfa04369e5eeb77109
jiyoung-Heo <jiyoungHeo.dev@gmail.com>
2021-11-27 22:44:11 +0900

    feat (makeAnswerNumber): 정답숫자 생성기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,36 @@
 package baseball;
 
+import java.util.ArrayList;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
-    }
+	public static void main(String[] args) {
+		Application application = new Application();
+		ArrayList<Integer> answerNumber = application.makeAnswerNumber();
+	}
+
+	/**
+	 * 서로 다른 3개의 난수 구하기
+	 * @return
+	 */
+	public ArrayList<Integer> makeAnswerNumber() {
+		ArrayList<Integer> answerNumber = new ArrayList<>();
+
+		while (true) {
+			int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+			if (answerNumber.contains(randomNumber)) {
+				continue;
+			} else {
+				answerNumber.add(randomNumber);
+			}
+
+			if (answerNumber.size() == 3) {
+				break;
+			}
+		}
+
+		return answerNumber;
+	}
 }
