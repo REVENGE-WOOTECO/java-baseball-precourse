9b449a541fb27299dc5cb6d51249aa27d61db8e1
highright96 <nsw5296@naver.com>
2021-11-29 20:32:32 +0900

    feat: Application 클래스 생성 (Controller 역할을 함)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,43 @@
 package baseball;
 
+import baseball.domain.BaseballGame;
+import baseball.domain.CommandType;
+import baseball.domain.Computer;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        do {
+            startGame();
+        } while (checkRestartGame());
+    }
+
+    private static void startGame() {
+        BaseballGame game = createBaseballGame();
+        do {
+            String playerInput = InputView.requestInputNumber();
+            String roundResult = game.roundStart(playerInput);
+            OutputView.printRoundResult(roundResult);
+        } while (checkEndGame(game));
+    }
+
+    private static BaseballGame createBaseballGame() {
+        Computer computer = Computer.create();
+        return BaseballGame.create(computer);
+    }
+
+    private static boolean checkEndGame(BaseballGame game) {
+        if (game.isGameEnd()) {
+            OutputView.printGameResult();
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean checkRestartGame() {
+        String playerInput = InputView.requestRestartInputNumber();
+        return CommandType.of(playerInput).equals(CommandType.RESTART);
     }
 }
