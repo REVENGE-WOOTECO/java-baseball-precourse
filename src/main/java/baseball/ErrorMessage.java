43d7f6e2c5ebde2d99637864f4c90c2116d8d4b5
highright96 <nsw5296@naver.com>
2021-11-29 20:31:27 +0900

    feat: ErrorMessage 클래스 생성

---
+++ src/main/java/baseball/ErrorMessage.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class ErrorMessage {
+
+    public static final String INVALID_INPUT_REQUEST = "잘못된 값을 입력했습니다.";
+    public static final String DUPLICATE_INPUT_REQUEST = "중복된 값을 입력했습니다.";
+}
