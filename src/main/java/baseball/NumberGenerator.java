34acb20297568ee83b2d5399268e296c89bcccb2
bbeomgeun <miki308@naver.com>
2021-11-30 04:03:30 +0900

    feat: 랜덤 3자리수 생성 기능 추가

---
+++ src/main/java/baseball/NumberGenerator.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
+
+import static baseball.Application.*;
+
+public class NumberGenerator {
+
+    public String numberGenerate(){
+        StringBuilder answer = new StringBuilder();
+        HashSet<Integer> duplicateCheck = new HashSet<>();
+
+        while (answer.length() != DIGIT){
+            int randomNumber = Randoms.pickNumberInRange(START_OF_NUMBER_RANGE, END_OF_NUMBER_RANGE);
+            if(!duplicateCheck.contains(randomNumber)){
+                duplicateCheck.add(randomNumber);
+                answer.append(randomNumber);
+            }
+        }
+        return answer.toString();
+    }
+}
