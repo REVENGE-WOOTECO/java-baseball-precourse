588f227ad848bcbfc0f473818a5107ec42494752
bbeomgeun <miki308@naver.com>
2021-11-30 22:27:05 +0900

    feat: 결과를 전달 받아 메세지를 출력하는 기능

---
+++ src/main/java/baseball/MessagePrint.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import static baseball.Application.ANSWER_COUNT;
+import static baseball.Application.NOTHING_COUNT;
+import static utils.Message.*;
+
+public class MessagePrint {
+    public String getResultMessage(int strikeCount, int ballCount) {
+        String resultMessage = "";
+        if (strikeCount == ANSWER_COUNT) {
+            resultMessage = strikeCount + STRIKE_MESSAGE + "\n" + CORRECT_MESSAGE;
+        } else if (strikeCount == NOTHING_COUNT && ballCount == NOTHING_COUNT) {
+            resultMessage = NOTHING_MESSAGE;
+        } else if (strikeCount > NOTHING_COUNT && ballCount > NOTHING_COUNT) {
+            resultMessage = ballCount + BALL_MESSAGE + " " + strikeCount + STRIKE_MESSAGE;
+        } else if (strikeCount == NOTHING_COUNT) {
+            resultMessage = ballCount + BALL_MESSAGE;
+        } else if (ballCount == NOTHING_COUNT) {
+            resultMessage = strikeCount + STRIKE_MESSAGE;
+        }
+
+        return resultMessage;
+    }
+}
