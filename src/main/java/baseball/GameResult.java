627c8f2d93bf819dc03d9ce2941819a60c178505
bbeomgeun <miki308@naver.com>
2021-11-30 22:25:58 +0900

    feat: 결과 계산 기능 추가

---
+++ src/main/java/baseball/GameResult.java
@@ -0,0 +1,43 @@
+package baseball;
+
+import java.util.HashMap;
+
+import static baseball.Application.DUP_COUNT;
+
+public class GameResult {
+    public int getStrikeCnt(String input, String answer) {
+        int strikeCount = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char inputChar = input.charAt(i);
+            char answerChar = answer.charAt(i);
+
+            if (Character.compare(inputChar, answerChar) == 0) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
+
+    public int getBallCnt(String input, String answer) {
+        HashMap<Character, Integer> ballCountMap = new HashMap<>();
+        int ballCount = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char inputChar = input.charAt(i);
+            char answerChar = answer.charAt(i);
+
+            if (Character.compare(inputChar, answerChar) != 0) {
+                ballCountMap.put(inputChar, ballCountMap.getOrDefault(inputChar, 0) + 1);
+                ballCountMap.put(answerChar, ballCountMap.getOrDefault(answerChar, 0) + 1);
+            }
+        }
+
+        for (Character c : ballCountMap.keySet()) {
+            if (ballCountMap.get(c) == DUP_COUNT) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    }
+}
