58a12f5d222ae5cf312e93467f6afead8c1e46c9
bbeomgeun <miki308@naver.com>
2021-11-30 21:05:10 +0900

    feat: 입력을 검증하는 validation 기능 추가

---
+++ src/main/java/baseball/Validation.java
@@ -0,0 +1,62 @@
+package baseball;
+
+import java.util.HashSet;
+
+import static baseball.Application.*;
+
+public class Validation {
+    public boolean validateInputNumber(String input) {
+
+        if (!isRightDigit(input)) {
+            return false;
+        }
+
+        if (!isNumber(input)) {
+            return false;
+        }
+
+        if (isDuplicate(input)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean validateContinueNumber(String input) {
+        if (input.equals(RESTART) || input.equals(EXIT)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isRightDigit(String input) {
+        if (input.length() != DIGIT) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean isNumber(String input) {
+        for (char c : input.toCharArray()) {
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isDuplicate(String input) {
+        HashSet<Character> duplicateCheck = new HashSet<>();
+
+        for(char c : input.toCharArray()) {
+            if(!duplicateCheck.contains(c)){
+                duplicateCheck.add(c);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
