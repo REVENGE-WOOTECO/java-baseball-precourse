e28cf2a4a548483847d9f7c3cb7421ff750975f9
moduhan <devduhanmo@gmail.com>
2021-12-01 22:04:30 +0900

    refactor: judgeResult 메서드로 스트라이크, 볼 판단

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,30 +7,41 @@ import java.util.List;
 public class Game {
 
     private final Util util;
+    private static final String STRIKE = "strike";
+    private static final String BALL = "ball";
+    private static final String NOTHING = "nothing";
 
     public Game(Util util) {
         this.util = util;
     }
 
-    String play(List<Integer> input, List<Integer> computer) {
-        String result = "";
+    public String play(List<Integer> input, List<Integer> computer) {
         int strike = 0;
         int ball = 0;
         for (int pos = 0; pos < util.getBaseCount(); pos++) {
-            if (isStrike(pos, input.get(pos), computer)) {
+            String judgeResult = judgeResult(pos, input.get(pos), computer);
+            if (judgeResult.equals(STRIKE)) {
                 strike++;
-            } else if (isBall(input.get(pos), computer)) {
+            } else if (judgeResult.equals(BALL)) {
                 ball++;
             }
         }
         printResult(strike, ball);
-        if (strike == 3) {
+        if (hasSuccess(strike)) {
+            return Console.readLine();
+        }
+        return "";
+    }
+
+    private boolean hasSuccess(int strike) {
+        if (strike == util.getBaseCount()) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
                     + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            result = Console.readLine();
+            return true;
         }
-        return result;
+        return false;
     }
+
 
     private void printResult(int strike, int ball) {
         if (strike != 0 && ball != 0) {
@@ -44,11 +55,12 @@ public class Game {
         }
     }
 
-    private boolean isStrike(int pos, int num, List<Integer> computer) {
-        return computer.indexOf(num) == pos;
-    }
-
-    private boolean isBall(int num, List<Integer> computer) {
-        return computer.contains(num);
+    private String judgeResult(int pos, int num, List<Integer> computer) {
+        if (computer.indexOf(num) == pos) {
+            return STRIKE;
+        } else if (computer.contains(num)) {
+            return BALL;
+        }
+        return NOTHING;
     }
 }
