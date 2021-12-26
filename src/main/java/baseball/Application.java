36d068f1a1239a4af0c2c4d2a1492873d9c5a540
Nylah <jiyoungHeo.dev@gmail.com>
2021-12-26 22:03:12 +0900

    refactor: 상수 선언

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.TreeSet;
 import java.util.stream.Stream;
 
@@ -8,11 +9,25 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
+	private final static String MESSAGE_END_GAME = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	private final static String MESSAGE_BALL = "볼";
+	private final static String MESSAGE_STRIKE = "스트라이크";
+	private final static String MESSAGE_NOTHING = "낫싱";
+	private static final String REQUEST_INPUT_NUMBER = "숫자를 입력해주세요: ";
+	private static final String REQUEST_END_OR_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+
+	private static final String COMMAND_RESTART_GAME = "1";
+	private static final String COMMAND_END_GAME = "2";
+	private static final int CONDITION_END = 3;
+	private static final int QUESTION_NUMBER_DIGITS = 3;
+	private static final char QUESTION_NUMBER_MIN = 1;
+	private static final char QUESTION_NUMBER_MAX = 9;
+
 	public static void main(String[] args) {
 		Application application = new Application();
 
 		while (true) {
-			ArrayList<Integer> answerNumber = application.makeAnswerNumber();
+			List<Integer> answerNumber = application.makeAnswerNumber();
 
 			boolean isAnswer;
 			do {
@@ -21,7 +36,7 @@ public class Application {
 				isAnswer = application.compareNumber(answerNumber, inputNumber);
 			} while (isAnswer == false);
 
-			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			System.out.println(CONDITION_END + MESSAGE_END_GAME);
 
 			String inputStartOver = application.getInputStartOver();
 			if (application.isStartOver(inputStartOver) == false) {
@@ -30,10 +45,10 @@ public class Application {
 		}
 	}
 
-	public ArrayList<Integer> makeAnswerNumber() {
-		ArrayList<Integer> answerNumber = new ArrayList<>();
+	public List<Integer> makeAnswerNumber() {
+		List<Integer> answerNumber = new ArrayList<>();
 
-		while (answerNumber.size() < 3) {
+		while (answerNumber.size() < CONDITION_END) {
 			int randomNumber = Randoms.pickNumberInRange(1, 9);
 
 			if (answerNumber.contains(randomNumber) == false) {
@@ -46,7 +61,7 @@ public class Application {
 	}
 
 	public int inputNumber() {
-		System.out.print("숫자를 입력해주세요: ");
+		System.out.print(REQUEST_INPUT_NUMBER);
 		String inputNumber = Console.readLine();
 
 		handleException(inputNumber);
@@ -66,45 +81,45 @@ public class Application {
 		char[] inputNumberSplitArray = inputNumber.toCharArray();
 
 		for (char c : inputNumberSplitArray) {
-			if (c >= '1' && c <= '9') {
+			if (c >= QUESTION_NUMBER_MIN && c <= QUESTION_NUMBER_MAX) {
 				inputNumberArray.add(Character.getNumericValue(c));
 			}
 		}
 
-		return inputNumberArray.size() != 3;
+		return inputNumberArray.size() != QUESTION_NUMBER_DIGITS;
 	}
 
-	public boolean compareNumber(ArrayList<Integer> answerNumber, int inputNumber) {
+	public boolean compareNumber(List<Integer> answerNumber, int inputNumber) {
 		String answer = hint(answerNumber, inputNumber);
 		System.out.println(answer);
 
-		return answer.equals("3스트라이크");
+		return answer.equals(CONDITION_END + MESSAGE_STRIKE);
 	}
 
-	public String hint(ArrayList<Integer> answerNumber, int inputNumber) {
+	public String hint(List<Integer> answerNumber, int inputNumber) {
 		int ballCount = ballCount(answerNumber, inputNumber);
 		int strikeCount = strikeCount(answerNumber, inputNumber);
 
 		String answer = "";
 
 		if (ballCount != 0) {
-			answer += ballCount + "볼";
+			answer += ballCount + MESSAGE_BALL;
 		}
 		if (strikeCount != 0) {
 			if (answer.isEmpty() == false) {
 				answer += " ";
 			}
-			answer += strikeCount + "스트라이크";
+			answer += strikeCount + MESSAGE_STRIKE;
 		}
 
 		if (ballCount == 0 && strikeCount == 0) {
-			answer = "낫싱";
+			answer = MESSAGE_NOTHING;
 		}
 
 		return answer;
 	}
 
-	public int ballCount(ArrayList<Integer> answerNumber, int inputNumber) {
+	public int ballCount(List<Integer> answerNumber, int inputNumber) {
 		int[] inputNumberArray = Stream.of(String.valueOf(inputNumber).split("")).mapToInt(Integer::parseInt).toArray();
 		int count = 0;
 
@@ -118,7 +133,7 @@ public class Application {
 		return count;
 	}
 
-	public int strikeCount(ArrayList<Integer> answerNumber, int inputNumber) {
+	public int strikeCount(List<Integer> answerNumber, int inputNumber) {
 		int[] inputNumberArray = Stream.of(String.valueOf(inputNumber).split("")).mapToInt(Integer::parseInt).toArray();
 
 		int count = 0;
@@ -133,14 +148,14 @@ public class Application {
 	}
 
 	public String getInputStartOver() {
-		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+		System.out.println(REQUEST_END_OR_RESTART);
 		return Console.readLine();
 	}
 
 	public boolean isStartOver(String inputNumber) {
-		if (inputNumber.equals("1")) {
+		if (inputNumber.equals(COMMAND_RESTART_GAME)) {
 			return true;
-		} else if (inputNumber.equals("2")) {
+		} else if (inputNumber.equals(COMMAND_END_GAME)) {
 			return false;
 		} else {
 			throw new IllegalArgumentException();
